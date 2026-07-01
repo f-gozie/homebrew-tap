e7413428546d592c083878761e0c45de352bdb31
Agozie Favour <fagozie43@gmail.com>
2026-07-01 23:19:43 +0100

    usageos 0.1.1

--- Casks/usageos.rb
+++ Casks/usageos.rb
@@ -1,6 +1,6 @@
 cask "usageos" do
-  version "0.1.0"
-  sha256 "2daa59cad382deedc7a5c0e79c79e5318445eb764eb917259d6a79679fe84dbb"
+  version "0.1.1"
+  sha256 "a4a19132cda79147949a770fbfd96035b55ffb992f5bac2609e9183c83bd006e"
 
   url "https://github.com/f-gozie/usage-os/releases/download/v#{version}/UsageOS-#{version}-universal.dmg",
       verified: "github.com/f-gozie/usage-os/"
